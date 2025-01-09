2935b8f73cd0cba62974af4fecdc8d364bf99ed4
Gary Li <gary.li@sap.com>
2025-01-09 11:01:28 +0800

    log added

--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # chat
-- Chat --express --socket.io
+- Chat 
+    - express
+    - socket.io
+    - winston
+- npm config get proxy && npm config get registry
 - npm install --save express --registry=https://registry.npmmirror.com
 - npm install --save socket.io --registry=https://registry.npmmirror.com
 - npm install nodemon --save-dev
+- log [both winston and debug are widely used]
+    - npm install winston | npm i winston | npm install --save winston
+    - npm install debug 
+        - In Windows CMD: `set DEBUG=app & node main.js` (Powershell does not work)
+        - alternatively add "process.env.DEBUG = 'app'" in main.js
 ## Execution
-- `npx nodemon main.js` or `npm start`
+- `npx nodemon main.js` or `npm start` 
--- main.js
+++ main.js
@@ -1,30 +1,53 @@
+process.env.DEBUG = 'app';
+
 const express = require('express');
 const http = require('http');
 const socketio = require('socket.io');
+const winston = require('winston');
+const debug = require('debug')('app');
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+// Set up winston logger
+const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.combine(
+    winston.format.timestamp(),
+    winston.format.printf(({ timestamp, level, message }) => {
+      return `${timestamp} [WINSTON] [${level.toUpperCase()}]: ${message}`;
+    })
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({ filename: 'app.log' })
+  ]
+});
+
 app.use(express.static(`${__dirname}/public`));
 
 io.on('connection', (socket) => {
-  console.log('New WebSocket connection');
-
-  // socket.emit('msg', 'Hi there!');
+  const socketId = socket.id;
+  logger.info(`New WebSocket connection: ${socketId}`);
+  debug(`New WebSocket connection: ${socketId}`);
 
   socket.on('msg', (message) => {
-    console.log('Message:', message);
-    io.emit("msg", `[User: ${socket.id}] ${message}`);
+    logger.info(`Message from ${socketId}: ${message}`);
+    debug(`Message from ${socketId}: ${message}`);
+    io.emit("msg", `[User: ${socketId}] ${message}`);
   });
 
   socket.on('disconnect', () => {
-    console.log('User disconnected');
+    logger.info(`User disconnected: ${socketId}`);
+    debug(`User disconnected: ${socketId}`);
   });
 });
 
 server.listen(8080, () => {
-  console.log('Server started on http://localhost:8080');
+  logger.info('Server started on http://localhost:8080');
+  debug('Server started on http://localhost:8080');
 }).on('error', (err) => {
-  console.error('Failed to start server:', err);
+  logger.error('Failed to start server:', err);
+  debug('Failed to start server:', err);
 });
